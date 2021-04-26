8b14cdd64ed060951d22ea797e62dfe298957abd
casey <casey.khaled@gmail.com>
2021-04-26 11:19:04 +0700

    Minor updates on partial messages

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.set('views', 'views');
 app.set('view engine', 'ejs');
